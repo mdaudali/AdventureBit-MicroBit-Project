e2a4f38d9162d4e7bd9eacbfa76aa4ef01137fc7
Mohammed Daudali <mdaudali786@gmail.com>
2016-12-01 11:15:38 +0000

    Sends an start fight and end fight command to prevent other interactions during the fight

--- enemy.py
+++ enemy.py
@@ -44,6 +44,8 @@ class Enemy:
 
     def take_damage(self, damage):
         self.health -= int(damage)
+        if self.health <= 0:
+            com.send_command("end_fight", 1)
 
     def send_health(self):
         com.send_command("health", self.health)
@@ -59,19 +61,26 @@ class Enemy:
     def aoe_attack(self):
         com.send_command("enemy_attack", self.level)
 
+    def wake(self):
+        com.send_command("start_fight", 1)
+        display.show(Image("00900:00900:00900:00000:00900"))
+        sleep(500)
+        for x in range(11):
+            display.show(Image.ANGRY.shift_right(x - 5))
+            sleep(200)
 
-def show_pixels(mapping):
-    grid = [["0"] * 5 for y in range(5)]
-    for x, y in mapping:
-        grid[x][y] = "9"
-    return Image(':'.join([''.join(vals) for vals in grid]))
 
 com = Comm(42)
 enemy = Enemy()
 while True:
+    resp = com.wait_for_command(0)
+    if resp["command"] == "start_fight":
+        while not resp["command"] == "end_fight":
+            resp = com.wait_for_command(0)
+            if button_a.is_pressed() or button_b.is_pressed():
+                display.scroll("Finish fight")
     if button_a.is_pressed() or button_b.is_pressed():
-        display.show("00900:00900:00900:00000:00000")
-        sleep(100)
+        enemy.wake()
         break
 while enemy.health > 0:
     display.show(enemy.display_health())
